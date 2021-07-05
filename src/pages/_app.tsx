7d6bac25e09d9a1835f6084064fd78650436f378
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-05 15:29:53 +0800

    🐛 Fix Eslint errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+// eslint-disable-next-line react/destructuring-assignment
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
 export default MyApp
