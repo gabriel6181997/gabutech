1133dacb46e69a5f49f2620a9115fb04e3de5f48
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-07 11:40:51 +0800

    ✨ Add color setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   purge: ["./src/**/*.{ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      blue: colors.sky,
+      gray:colors.gray,
+      pink: colors.pink,
+    },
     extend: {},
   },
   variants: {
