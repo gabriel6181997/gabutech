2ec5876a0121283910a6b9f4f0987ad75cd8fa59
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-09 19:35:09 +0800

    🎨 Adjust eslint and prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,18 +18,12 @@ module.exports = {
   ],
   rules: {
     "no-console": ["error", { allow: ["warn", "info", "error"] }],
-    "no-restricted-syntax": [
-      "error",
-      { selector: "TSEnumDeclaration", message: "Don't declare enums" },
-    ],
+    "no-restricted-syntax": ["error", { selector: "TSEnumDeclaration", message: "Don't declare enums" }],
     "prefer-arrow-callback": "error",
     "prefer-const": "error",
     "func-style": ["error", "expression"],
     "arrow-body-style": ["error", "always"],
-    "no-restricted-imports": [
-      "error",
-      { paths: [{ name: "react", importNames: ["default"] }] },
-    ],
+    "no-restricted-imports": ["error", { paths: [{ name: "react", importNames: ["default"] }] }],
     "react/prop-types": "off",
     "react/react-in-jsx-scope": "off",
     "react/display-name": "error",
@@ -55,10 +49,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      { prefer: "type-imports" },
-    ],
+    "@typescript-eslint/consistent-type-imports": ["warn", { prefer: "type-imports" }],
     "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/naming-convention": [
       "error",
@@ -83,5 +74,9 @@ module.exports = {
       },
     ],
   },
-  overrides: [{ files: ["src/pages/**/*.tsx"], rules: { "import/no-default-export": "off" } }],
+  overrides: [
+    {
+      files: ["src/pages/**/*.tsx"], rules: { "import/no-default-export": "off" },
+    },
+  ],
 };
