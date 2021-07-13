8773404a94edd742916791b0b5397aa98604df59
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-13 21:13:15 +0800

    📱 Add responsive design for header

--- src/components/separate/Header.tsx
+++ src/components/separate/Header.tsx
@@ -19,9 +19,9 @@ const NAVITEMS = [
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 right-0 left-0 z-50 dark:text-white bg-white dark:bg-gray-900">
+    <header className="fixed top-0 right-0 left-0 z-50 h-10 md:h-20 dark:text-white bg-white dark:bg-gray-900">
       <div className="flex items-center mx-auto max-w-screen-xl">
-        <h1>
+        <h1 className="m-2">
           <Link href="/">
             <a>
               <Image src="/img/logo.png" alt="Gabutech" width={120} height={30} />
@@ -35,10 +35,10 @@ export const Header = () => {
                 return (
                   <li
                     key={navitem.title}
-                    className="relative hover:after:absolute hover:after:top-[65%] hover:after:left-0 mr-10 last:ml-0 hover:even:after:w-9 hover:after:w-11 h-20 hover:after:h-[2px] leading-[80px] hover:text-blue-300 hover:after:bg-blue-300 duration-300 hover:after:-translate-y-2/4"
+                    className="hidden md:block"
                   >
                     <Link href={navitem.link}>
-                      <a>{navitem.title}</a>
+                      <a className="relative hover:after:absolute hover:after:bottom-[5%] hover:after:left-0 mr-10 last:ml-0 hover:even:after:w-9 hover:after:w-11 h-20 hover:after:h-[2px] leading-[80px] hover:text-blue-300 hover:after:bg-blue-300 duration-300 hover:after:-translate-y-2/4">{navitem.title}</a>
                     </Link>
                   </li>
                 );
@@ -46,7 +46,7 @@ export const Header = () => {
             </ul>
           </nav>
 
-          <div className="ml-5">
+          <div className="my-2 ml-5">
             <DarkModeSwitch />
           </div>
         </div>
