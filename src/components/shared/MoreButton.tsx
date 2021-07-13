b52fa984a37bd9ed341fa6e5d39e51d4af6db324
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-13 12:03:06 +0800

    🚚 Change Button to MoreButton

--- src/components/shared/MoreButton.tsx
+++ src/components/shared/MoreButton.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import type { VFC } from "react";
 
-export const Button = () => {
+type Props = {
+  href: string;
+}
+
+export const MoreButton: VFC<Props> = (props) => {
   return (
-    <Link href="/">
+    <Link href={props.href}>
       <a className="inline-block p-3 py-3 px-10 font-bold tracking-widest text-blue-300 hover:text-white bg-white hover:bg-blue-300 rounded-xl shadow-md transition-colors duration-300">
         MORE
       </a>
