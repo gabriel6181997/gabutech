f9bb293ada8f8d2b14b9f197e7218b788353e310
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-15 15:44:07 +0800

    ✨ Add Types

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,6 +7,11 @@ export type Blog = {
     revisedAt: string;
     updatedAt: string;
   };
+  image: {
+    url: string;
+    height: number;
+    width: number;
+  }
   content: string;
   createdAt: string;
   id: string;
@@ -18,4 +23,60 @@ export type Blog = {
 
 export type Blogs = {
   contents: Blog[];
-}
+  limit: number;
+  offset: number;
+  totalCount: number;
+};
+
+export type Top = {
+  top: {
+    createdAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    top: {
+      aboutdescription: string;
+      catchcopy: string;
+      fieldId: string;
+      firstviewimage: {
+        height: number;
+        url: string;
+        width: number;
+      };
+      selfportrait: {
+        height: number;
+        url: string;
+        width: number;
+      };
+      title: string;
+    };
+  };
+};
+
+export type AboutType = {
+  about: {
+    about: {
+      conclusion: string;
+      fieldId: string;
+      history: History[];
+      shortdescription: string;
+      skill: string;
+      thumbnail: {
+        height: number;
+        url: string;
+        width: number;
+      };
+    };
+    createdAt: string;
+    publishedAt: string;
+    revisedAt: string;
+    updatedAt: string;
+  };
+};
+
+export type History = {
+  description: string;
+  fieldId: string;
+  month?: string;
+  title: string;
+  year: string;
+};
