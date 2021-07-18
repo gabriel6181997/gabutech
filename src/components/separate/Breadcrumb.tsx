973196ca5bc98891ac099b472850e75587c466f9
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-18 12:18:51 +0800

    ✨ Add Link to Top page

--- src/components/separate/Breadcrumb.tsx
+++ src/components/separate/Breadcrumb.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export const Breadcrumb = () => {
   return (
     <div className="container my-3">
       <ul className="flex">
-        <li>Home {">"}</li>
+        <li>
+          <Link href="/">
+            <a className="hover:text-blue-300 hover:underline transition-all duration-300">Home</a>
+          </Link>
+        </li>
+        <li className="ml-2">{">"}</li>
         <li className="ml-2">About</li>
       </ul>
     </div>
