a1f15d6774672e5d45d81f5dfd13deda51fba585
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-20 18:14:18 +0800

    💄 Change sequence of components in Sidebar

--- src/components/layouts/sidebar/Sidebar.tsx
+++ src/components/layouts/sidebar/Sidebar.tsx
@@ -1,15 +1,19 @@
 import { Archivecard } from "src/components/layouts/sidebar/Archivecard";
 import { Profilecard } from "src/components/layouts/sidebar/Profilecard";
-import { Searchbox } from "src/components/layouts/sidebar/Searchbar";
+import { Searchbox } from "src/components/layouts/sidebar/Searchbox";
 import { Tagcard } from "src/components/layouts/sidebar/Tagcard";
 
 export const Sidebar = () => {
   return (
     <aside className="block">
-      <Searchbox />
+      <div className="flex flex-col-reverse md:flex-col">
+        <div className="mt-10 md:mt-0">
+          <Searchbox />
+        </div>
 
-      <div className="mt-10">
-         <Profilecard />
+        <div className="mt-10">
+          <Profilecard />
+        </div>
       </div>
 
       <div className="mt-10">
