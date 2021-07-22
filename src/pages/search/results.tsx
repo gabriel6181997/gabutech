32d096c8b7c6090d647c35abbf46163b20ea5522
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-22 11:25:17 +0800

    ✨ Add results page

---
+++ src/pages/search/results.tsx
@@ -0,0 +1,43 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/dist/client/router";
+import { useEffect, useState } from "react";
+import { Card } from "src/components/blogandwork/Card";
+import { Layout } from "src/components/layouts/Layout";
+import { Title } from "src/components/layouts/Title";
+import { client } from "src/libs/client";
+import type { Blogs } from "src/types/types";
+
+const ResultsPage: NextPage = () => {
+  const router = useRouter();
+  const searchKeyword = router.query.keyword;
+  const [results, setResults] = useState<Blogs>();
+  useEffect(() => {
+    async () => {
+      const data: Blogs = await client.get({ endpoint: `blogs?q=${searchKeyword}` });
+      setResults(data);
+    };
+  }, [searchKeyword]);
+
+  return (
+    <Layout>
+      <Title variant="box" className="text-3xl md:text-4xl" bigTitle>
+        {searchKeyword}の検索結果
+      </Title>
+      <ul>
+        {results?.contents.map((result) => {
+          return (
+            <Card
+              key={result.title}
+              href={`/blog/${result.id}`}
+              image={result.image.url}
+              title={result.title}
+              date={result.createdAt}
+            />
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+export default ResultsPage;
