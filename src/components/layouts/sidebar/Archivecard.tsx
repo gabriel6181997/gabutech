39c5c3cd8471d8d49c746d2a79d7a1b5a68633b6
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-30 09:28:11 +0800

    🎨 Change format of blogDate

--- src/components/layouts/sidebar/Archivecard.tsx
+++ src/components/layouts/sidebar/Archivecard.tsx
@@ -28,8 +28,8 @@ export const Archivecard = () => {
   const blogDate = blogInfo?.contents?.map((blog) => {
     const date = new Date(blog.publishedAt);
     const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const fixedMonth = `${year}年${month}月`;
+    const month = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+    const fixedMonth = `${year}-${month}`;
     return fixedMonth;
   });
 
