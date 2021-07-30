fe63b0e57d5cb76a2f327c3b86de61ad98d61d78
gabriel6181997 <gabriel6181997@gmail.com>
2021-07-30 10:54:37 +0800

    ♻️ Change data fetching code and add loading

--- src/pages/search/results.tsx
+++ src/pages/search/results.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import ReactLoading from "react-loading";
 import { Card } from "src/components/blogandwork/Card";
 import { Layout } from "src/components/layouts/Layout";
 import { Title } from "src/components/layouts/Title";
@@ -11,13 +12,23 @@ const ResultsPage: NextPage = () => {
   const router = useRouter();
   const searchKeyword = router.query.keyword;
   const [results, setResults] = useState<Blogs>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getBlogInfo = useCallback(async () => {
+    try {
+      const data: Blogs = await client.get({
+        endpoint: `blogs?q=${searchKeyword}`,
+      });
+      setResults(data);
+    } catch (e) {
+      throw new Error(`ブログの情報を取得できませんでした！`);
+    }
+    setIsLoading(false);
+  }, [searchKeyword]);
 
   useEffect(() => {
-    (async () => {
-      const data: Blogs = await client.get({ endpoint: `blogs?q=${searchKeyword}` });
-      setResults(data);
-    })();
-  }, [searchKeyword]);
+    getBlogInfo();
+  }, [getBlogInfo]);
 
   return (
     <Layout>
@@ -25,7 +36,9 @@ const ResultsPage: NextPage = () => {
         {searchKeyword}の検索結果
       </Title>
       <ul className="flex flex-wrap gap-10 justify-center mt-10">
-        {(results?.contents.length ?? 0) > 0 ? (
+        {isLoading ? (
+          <ReactLoading type="spin" color="#BFDBFE" height={"10%"} width={"10%"} />
+        ) : (results?.contents.length ?? 0) > 0 ? (
           results?.contents.map((result) => {
             return (
               <Card
