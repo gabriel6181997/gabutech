e7c073ad10a7cce8f08efadf302fa977ae650240
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-01 14:41:21 +0800

    🔥 Remove NextSeo code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import type { DocumentContext } from "next/document";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { NextSeo } from "next-seo";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -11,19 +10,6 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="ja">
-        <NextSeo
-          title="Gabutech"
-          description="ガブリエルの自己紹介・ブログ・プロダクト紹介のまとめサイト"
-          openGraph={{
-            url: `https://gabutech.vercel.app/`,
-            images: [{ url: "/img/logo.png", alt: "Gabutech" }],
-          }}
-          twitter={{
-            handle: "@gabu_ITengineer",
-            site: "https://gabutech.vercel.app/",
-            cardType: "summary",
-          }}
-        />
         <Head />
         <body className="h-screen md:h-full">
           <Main />
