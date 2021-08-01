f33fa20c467b45d51470bf507a3c7de26cc9dded
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-01 14:41:52 +0800

    ✨ Add NextSeo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,30 @@
-import 'src/styles/globals.css'
+import "src/styles/globals.css";
 
-import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
+import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
+import { ThemeProvider } from "next-themes";
 
 // eslint-disable-next-line react/destructuring-assignment
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class">
+      <NextSeo
+        title="Gabutech"
+        description="ガブリエルの自己紹介・ブログ・プロダクト紹介のまとめサイト"
+        openGraph={{
+          url: `https://gabutech.vercel.app/`,
+          images: [{ url: "/img/logo.png", alt: "Gabutech" }],
+        }}
+        twitter={{
+          handle: "@gabu_ITengineer",
+          site: "https://gabutech.vercel.app/",
+          cardType: "summary",
+        }}
+      />
+
       <Component {...pageProps} />
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
