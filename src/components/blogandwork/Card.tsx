52e44981522a4d6b31a27160bef1951b505da4ba
gabriel6181997 <gabriel6181997@gmail.com>
2021-08-10 19:38:35 +0800

    💄 Change border radius of Image

--- src/components/blogandwork/Card.tsx
+++ src/components/blogandwork/Card.tsx
@@ -8,23 +8,20 @@ type Props = {
   image: string;
   title: string;
   date: string;
-}
+};
 
-export const Card:VFC<Props> = (props) => {
+export const Card: VFC<Props> = (props) => {
   return (
     <li className="mx-auto md:mx-0 max-w-[250px] bg-white rounded-[10px] shadow-md">
       <Link href={props.href}>
         <a>
-          <Image src={props.image}
-            alt={props.title} width={250} height={250} />
+          <Image src={props.image} alt={props.title} width={250} height={250} className="rounded-t-[10px]" />
           <div className="pb-2 pl-3">
             <h3 className="font-bold">{props.title}</h3>
-            <p className="text-sm">
-              {fixDateFormat(props.date)}
-            </p>
+            <p className="text-sm">{fixDateFormat(props.date)}</p>
           </div>
         </a>
       </Link>
     </li>
   );
-}
+};
